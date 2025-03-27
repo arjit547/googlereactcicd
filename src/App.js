839ff78d8f4733a666134a4e198ebaae3fc181e3
Arjit <ks5015232@gmail.com>
2025-03-27 10:11:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-green-600">Our Website is Live! ✅</h1>
+    </div>
+  );
+};
+
+export default App;
