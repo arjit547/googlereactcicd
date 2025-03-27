de4d798d2e20058a30c6c5d930cef855c7d0b778
arjit547 <97424724+arjit547@users.noreply.github.com>
2025-03-27 10:34:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-green-600">Website is Live on Google Cloud! ✅</h1>
+      <h1 className="text-3xl font-bold text-green-600">Website is Live ! ✅</h1>
     </div>
   );
 };
